81cccae7e77cc3f3cf314adce2dd585410198485
Charlie Zhang <charliezhangbusiness@gmail.com>
2026-04-28 21:26:09 +0100

    Add: is-anagram - submission-60

---
+++ Data Structures & Algorithms/is-anagram/submission-60.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        smap, tmap = {}, {}
+
+        for i in range(len(s)):
+            smap[i] = 1 + smap[s[i], 0]
+            tmap[i] = 1 + tmap(t[i], 0)
+            
+
+            return smap == tmap
