c5c5123b8682d185fa5ecff737f4efb30d9a9ee6
Charlie Zhang <charliezhangbusiness@gmail.com>
2026-07-02 15:49:42 +0100

    Add: is-palindrome - submission-16

---
+++ Data Structures & Algorithms/is-palindrome/submission-16.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        # while l < r:
+        #         while l < r and not self.al
+        
+
+        print(dir(str))
