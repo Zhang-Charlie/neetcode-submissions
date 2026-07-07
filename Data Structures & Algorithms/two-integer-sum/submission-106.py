5d56706c37d1f381ddf905058c03d9ed0be57e01
Charlie Zhang <charliezhangbusiness@gmail.com>
2026-07-07 13:57:11 +0100

    Add: two-integer-sum - submission-106

---
+++ Data Structures & Algorithms/two-integer-sum/submission-106.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        print(dir(set))
+
+        seen = set()
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+
+            if diff in seen:
+                return [seen(diff), i]
+
+            seen.add(nums[i])
+
+    
+
+        
